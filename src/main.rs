cfd33f4dacc48929aba29f0712fadda65262b2f1
Lakshay Rajput <rajputlakshay553@gmail.com>
2024-01-18 05:52:06 +0530

    initial Commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+mod api;
+
+use api::task::{
+    get_task
+};
+use actix_web::{HttpServer,App,web::Data,middleware::Logger};
+
+#[actix_web::main]
+async fn main()->std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
+    HttpServer::new(move || {
+        let logger = new Logger::default();
+        App::new()
+        .wrap(logger)
+        .service(get_task)
+    })
+    .bind(("127.0.0.1",108))?
+    .run()
+    .await
+}
